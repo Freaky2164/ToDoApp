a71151133af0432875725628df8ef5fd7cb29367
Paul-Louis Faller <p.faller@seeburger.de>
2024-03-27 18:36:03 +0100

    Implemented service functionality for user

--- 2-todoapp-application/src/main/java/de/dhbw/ase/todoapp/application/UserService.java
+++ 2-todoapp-application/src/main/java/de/dhbw/ase/todoapp/application/UserService.java
@@ -1,12 +1,17 @@
 package de.dhbw.ase.todoapp.application;
 
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import de.dhbw.ase.todoapp.domain.entities.user.User;
+import de.dhbw.ase.todoapp.domain.entities.user.UserFactory;
 import de.dhbw.ase.todoapp.domain.entities.user.UserRepository;
+import de.dhbw.ase.todoapp.domain.exceptions.InvalidLoginException;
 import de.dhbw.ase.todoapp.domain.vo.Email;
+import de.dhbw.ase.todoapp.domain.vo.Password;
 
 
 @Service
@@ -21,14 +26,41 @@ public class UserService
     }
 
 
-    public User registerUser(final User user)
+    public User findUserById(final UUID userId)
     {
-        return userRepository.createUser(user);
+        return userRepository.findUserById(userId);
     }
 
 
-    public User findUserByMail(final Email mail)
+    public User findUserByMail(final String mailAdress)
     {
-        return userRepository.findUserByMail(mail);
+        return userRepository.findUserByMail(new Email(mailAdress));
+    }
+
+
+    public User registerUser(final String mailAdress, final String password)
+    {
+        if (mailAdress.isEmpty() || password.isEmpty())
+        {
+            throw new InvalidLoginException("Mail adress or password cannot be empty.");
+        }
+        if (userRepository.findUserByMail(new Email(mailAdress)) != null)
+        {
+            throw new InvalidLoginException("There already exists a user with the provided mail adress.");
+        }
+        User newUser = UserFactory.createUser(mailAdress, password);
+        return userRepository.createUser(newUser);
+    }
+
+
+    public UUID authenticateUser(final String mailAdress, final String password)
+    {
+        User user = userRepository.findUserByMail(new Email(mailAdress));
+        if (user != null)
+        {
+            Password storedPassword = new Password(password, user.getPassword().getSalt());
+            return storedPassword.equals(user.getPassword()) ? user.getId() : null;
+        }
+        return null;
     }
 }
